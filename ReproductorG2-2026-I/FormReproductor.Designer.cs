91be5a69cfd9be1a1e84f79574ebaf92e7c92268
faraday58 <jarodriguezvera@gmail.com>
2025-11-12 21:53:58 -0600

    Salvar y leer archivos

--- ReproductorG2-2026-I/FormReproductor.Designer.cs
+++ ReproductorG2-2026-I/FormReproductor.Designer.cs
@@ -28,7 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormReproductor));
             lstbPlayList = new ListBox();
+            cMenuStripReproductor = new ContextMenuStrip(components);
+            eliminarCanciónToolStripMenuItem = new ToolStripMenuItem();
             ptbAlbum = new PictureBox();
             label1 = new Label();
             txtbTitle = new TextBox();
@@ -38,14 +42,29 @@
             txtbArtist = new TextBox();
             menuStrip1 = new MenuStrip();
             archivoToolStripMenuItem = new ToolStripMenuItem();
+            guardarListaDeReproducciónToolStripMenuItem = new ToolStripMenuItem();
+            importarToolStripMenuItem = new ToolStripMenuItem();
             listaDeReproducciónToolStripMenuItem = new ToolStripMenuItem();
             agregarToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem1 = new ToolStripSeparator();
+            reproducirToolStripMenuItem = new ToolStripMenuItem();
+            pausarToolStripMenuItem = new ToolStripMenuItem();
+            detenerToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem2 = new ToolStripSeparator();
+            siguienteToolStripMenuItem = new ToolStripMenuItem();
+            anteriorToolStripMenuItem = new ToolStripMenuItem();
+            lbStartTime = new Label();
+            lbFinalTime = new Label();
+            pgbTime = new ProgressBar();
+            timer1 = new System.Windows.Forms.Timer(components);
+            cMenuStripReproductor.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)ptbAlbum).BeginInit();
             menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // lstbPlayList
             // 
+            lstbPlayList.ContextMenuStrip = cMenuStripReproductor;
             lstbPlayList.FormattingEnabled = true;
             lstbPlayList.Location = new Point(63, 110);
             lstbPlayList.Name = "lstbPlayList";
@@ -53,9 +72,22 @@
             lstbPlayList.TabIndex = 0;
             lstbPlayList.SelectedIndexChanged += lstbPlayList_SelectedIndexChanged;
             // 
+            // cMenuStripReproductor
+            // 
+            cMenuStripReproductor.ImageScalingSize = new Size(24, 24);
+            cMenuStripReproductor.Items.AddRange(new ToolStripItem[] { eliminarCanciónToolStripMenuItem });
+            cMenuStripReproductor.Name = "cMenuStripReproductor";
+            cMenuStripReproductor.Size = new Size(212, 36);
+            // 
+            // eliminarCanciónToolStripMenuItem
+            // 
+            eliminarCanciónToolStripMenuItem.Name = "eliminarCanciónToolStripMenuItem";
+            eliminarCanciónToolStripMenuItem.Size = new Size(211, 32);
+            eliminarCanciónToolStripMenuItem.Text = "Eliminar canción";
+            // 
             // ptbAlbum
             // 
-            ptbAlbum.Image = Properties.Resources.takeonme;
+            ptbAlbum.Image = Properties.Resources._default;
             ptbAlbum.Location = new Point(1046, 121);
             ptbAlbum.Name = "ptbAlbum";
             ptbAlbum.Size = new Size(344, 377);
@@ -126,13 +158,28 @@
             // 
             // archivoToolStripMenuItem
             // 
+            archivoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { guardarListaDeReproducciónToolStripMenuItem, importarToolStripMenuItem });
             archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
             archivoToolStripMenuItem.Size = new Size(88, 29);
             archivoToolStripMenuItem.Text = "Archivo";
             // 
+            // guardarListaDeReproducciónToolStripMenuItem
+            // 
+            guardarListaDeReproducciónToolStripMenuItem.Name = "guardarListaDeReproducciónToolStripMenuItem";
+            guardarListaDeReproducciónToolStripMenuItem.Size = new Size(356, 34);
+            guardarListaDeReproducciónToolStripMenuItem.Text = "Guardar Lista de Reproducción";
+            guardarListaDeReproducciónToolStripMenuItem.Click += guardarListaDeReproducciónToolStripMenuItem_Click;
+            // 
+            // importarToolStripMenuItem
+            // 
+            importarToolStripMenuItem.Name = "importarToolStripMenuItem";
+            importarToolStripMenuItem.Size = new Size(356, 34);
+            importarToolStripMenuItem.Text = "Importar";
+            importarToolStripMenuItem.Click += importarToolStripMenuItem_Click;
+            // 
             // listaDeReproducciónToolStripMenuItem
             // 
-            listaDeReproducciónToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { agregarToolStripMenuItem });
+            listaDeReproducciónToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { agregarToolStripMenuItem, toolStripMenuItem1, reproducirToolStripMenuItem, pausarToolStripMenuItem, detenerToolStripMenuItem, toolStripMenuItem2, siguienteToolStripMenuItem, anteriorToolStripMenuItem });
             listaDeReproducciónToolStripMenuItem.Name = "listaDeReproducciónToolStripMenuItem";
             listaDeReproducciónToolStripMenuItem.Size = new Size(202, 29);
             listaDeReproducciónToolStripMenuItem.Text = "Lista de Reproducción";
@@ -140,16 +187,92 @@
             // agregarToolStripMenuItem
             // 
             agregarToolStripMenuItem.Name = "agregarToolStripMenuItem";
-            agregarToolStripMenuItem.Size = new Size(178, 34);
+            agregarToolStripMenuItem.Size = new Size(200, 34);
             agregarToolStripMenuItem.Text = "Agregar";
             agregarToolStripMenuItem.Click += agregarToolStripMenuItem_Click;
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(197, 6);
+            // 
+            // reproducirToolStripMenuItem
+            // 
+            reproducirToolStripMenuItem.Image = (Image)resources.GetObject("reproducirToolStripMenuItem.Image");
+            reproducirToolStripMenuItem.Name = "reproducirToolStripMenuItem";
+            reproducirToolStripMenuItem.Size = new Size(200, 34);
+            reproducirToolStripMenuItem.Text = "Reproducir";
+            reproducirToolStripMenuItem.Click += reproducirToolStripMenuItem_Click;
+            // 
+            // pausarToolStripMenuItem
+            // 
+            pausarToolStripMenuItem.Name = "pausarToolStripMenuItem";
+            pausarToolStripMenuItem.Size = new Size(200, 34);
+            pausarToolStripMenuItem.Text = "Pausar";
+            // 
+            // detenerToolStripMenuItem
+            // 
+            detenerToolStripMenuItem.Image = (Image)resources.GetObject("detenerToolStripMenuItem.Image");
+            detenerToolStripMenuItem.Name = "detenerToolStripMenuItem";
+            detenerToolStripMenuItem.Size = new Size(200, 34);
+            detenerToolStripMenuItem.Text = "Detener";
+            // 
+            // toolStripMenuItem2
+            // 
+            toolStripMenuItem2.Name = "toolStripMenuItem2";
+            toolStripMenuItem2.Size = new Size(197, 6);
+            // 
+            // siguienteToolStripMenuItem
+            // 
+            siguienteToolStripMenuItem.Name = "siguienteToolStripMenuItem";
+            siguienteToolStripMenuItem.Size = new Size(200, 34);
+            siguienteToolStripMenuItem.Text = "Siguiente";
+            // 
+            // anteriorToolStripMenuItem
+            // 
+            anteriorToolStripMenuItem.Name = "anteriorToolStripMenuItem";
+            anteriorToolStripMenuItem.Size = new Size(200, 34);
+            anteriorToolStripMenuItem.Text = "Anterior";
+            // 
+            // lbStartTime
+            // 
+            lbStartTime.AutoSize = true;
+            lbStartTime.Location = new Point(475, 459);
+            lbStartTime.Name = "lbStartTime";
+            lbStartTime.Size = new Size(27, 25);
+            lbStartTime.TabIndex = 2;
+            lbStartTime.Text = "0 ";
+            // 
+            // lbFinalTime
+            // 
+            lbFinalTime.AutoSize = true;
+            lbFinalTime.Location = new Point(916, 463);
+            lbFinalTime.Name = "lbFinalTime";
+            lbFinalTime.Size = new Size(51, 25);
+            lbFinalTime.TabIndex = 2;
+            lbFinalTime.Text = "2:50 ";
+            // 
+            // pgbTime
+            // 
+            pgbTime.Location = new Point(513, 473);
+            pgbTime.Name = "pgbTime";
+            pgbTime.Size = new Size(391, 11);
+            pgbTime.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
             // 
             // FormReproductor
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(30, 215, 96);
             ClientSize = new Size(1568, 774);
+            Controls.Add(pgbTime);
             Controls.Add(txtbArtist);
+            Controls.Add(lbFinalTime);
+            Controls.Add(lbStartTime);
             Controls.Add(label3);
             Controls.Add(txtbAlbum);
             Controls.Add(label2);
@@ -158,10 +281,12 @@
             Controls.Add(ptbAlbum);
             Controls.Add(lstbPlayList);
             Controls.Add(menuStrip1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             MainMenuStrip = menuStrip1;
             Name = "FormReproductor";
             Text = "Reproductor";
             Load += FormReproductor_Load;
+            cMenuStripReproductor.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)ptbAlbum).EndInit();
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
@@ -183,5 +308,21 @@
         private ToolStripMenuItem archivoToolStripMenuItem;
         private ToolStripMenuItem listaDeReproducciónToolStripMenuItem;
         private ToolStripMenuItem agregarToolStripMenuItem;
+        private Label lbStartTime;
+        private Label lbFinalTime;
+        private ProgressBar pgbTime;
+        private ToolStripSeparator toolStripMenuItem1;
+        private ToolStripMenuItem reproducirToolStripMenuItem;
+        private ToolStripMenuItem pausarToolStripMenuItem;
+        private ToolStripMenuItem detenerToolStripMenuItem;
+        private ToolStripSeparator toolStripMenuItem2;
+        private ToolStripMenuItem siguienteToolStripMenuItem;
+        private ToolStripMenuItem anteriorToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem eliminarCanciónToolStripMenuItem;
+        private ContextMenuStrip cMenuStripReproductor;
+        private ToolStripMenuItem guardarListaDeReproducciónToolStripMenuItem;
+        private ToolStripMenuItem importarToolStripMenuItem;
     }
 }
--- ReproductorG2-2026-I/FormReproductor.cs
+++ ReproductorG2-2026-I/FormReproductor.cs
@@ -1,9 +1,13 @@
-using System.Collections.Generic;
+using System.IO;
+using System.Web;
+
+
 namespace ReproductorG2_2026_I
 {
     public partial class FormReproductor : Form
     {
         List<Musica> playList;
+        private int time;
         public FormReproductor()
         {
             playList = new List<Musica>();
@@ -15,7 +19,7 @@ namespace ReproductorG2_2026_I
         private void LoadMusicData()
         {
             // Aquí puedes cargar los datos de la música en el reproductor
-            playList.Add(new Musica("Desvelado", "Boby Pulido", "Desvelado",@"..\..\..\Resources\desvelado.jpeg"));
+            playList.Add(new Musica("Desvelado", "Boby Pulido", "Desvelado", @"..\..\..\Resources\desvelado.jpeg"));
             playList.Add(new Musica("Viento", "Caifanes", "Caifanes", @"..\..\..\Resources\caifanes.jpeg"));
             playList.Add(new Musica("Take On Me", "A-Ha", "Hunting High and Low", @"..\..\..\Resources\takeonme.jpeg"));
         }
@@ -47,6 +51,111 @@ namespace ReproductorG2_2026_I
             FormAgregar formAgregar = new FormAgregar();
             formAgregar.AddSong += AddMusicToPlaylist;
             formAgregar.ShowDialog();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lbStartTime.Text = time.ToString();
+            pgbTime.Value = time;
+
+
+            if (time == 100)
+            {
+                timer1.Stop();
+            }
+            time++;
+
+        }
+
+        private void reproducirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void guardarListaDeReproducciónToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Archivos de texto|*.txt|Archivos separados por ','| .csv";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter writer = null;
+                try
+                {
+                    writer = new StreamWriter(sfd.FileName);
+                    writer.WriteLine("Title,Artist,Album ");
+
+                    foreach (var musica in playList)
+                    {
+                        writer.WriteLine($"{musica.Title},{musica.Artist},{musica.Album}");
+                    }
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al guardar el archivo: " + ex.Message);
+
+                }
+                finally
+                {
+                    // Cerrar el escritor si es necesario
+                    writer?.Close();
+                }
+            }
+
+        }
+
+        private void importarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Archivos de texto|*.txt|Archivos separados por ','| .csv";
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader reader = null;
+                try
+                {
+                    reader = new StreamReader(ofd.FileName);
+
+                    string headerLine = reader.ReadLine(); // Leer la línea de encabezado
+
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        string[] columns = line.Split(',');
+
+                        if (columns.Length >= 3)
+                        {
+                            string title = columns[0];
+                            string artist = columns[1];
+                            string album = columns[2];
+                            Musica musica = new Musica(title, artist, album, @"..\..\..\Resources\default.jpeg");
+                            AddMusicToPlaylist(musica);
+                        }
+
+                    }
+
+                    lstbPlayList.Refresh();
+
+                    foreach (var musica in playList)
+                    {
+                        lstbPlayList.Items.Add(musica.Title);
+
+                    }
+                }
+                catch (Exception IO)
+                {
+                    MessageBox.Show("Error al leer el archivo: " + IO.Message);
+                }
+                finally
+                {
+                    reader?.Close();
+                }   
+
+
+            }
+
+
+
         }
     }
 }
